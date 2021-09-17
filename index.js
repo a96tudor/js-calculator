55e9ef9bcc40a3dd9f7b4a14e3ddaee88efc664d
Tudor Avram <t.avram@eyeo.com>
2021-09-17 17:15:00 +0200

    Solved Problem #4

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let pressedEqual = false;
 let lastValueEntered = null;
 let lastSign = null;
 let pressedDigit = false;
+let previewValues = [];
 
 const digits = [...Array(10).keys()].map((key) => key.toString());
 
@@ -51,6 +52,9 @@ const elements = {
     })(),
     get calculateButton() {
         return document.getElementById("btn-calculate");
+    },
+    get preview() {
+        return document.getElementById("preview");
     }
 }
 
@@ -85,30 +89,53 @@ function calculateOnEqual() {
     return operations[lastOpPressed](first, lastSign * second);
 }
 
+function addPreviewValues(sign, opCode) {
+    if (!opCode)
+        return
+    let value = elements.display.textContent;
+    if (value == "")
+        return
+    if (sign == -1 && !pressedEqual)
+        // Displaying negative values as (-1) instead of -1 in order
+        // to improve preview readibility.
+        previewValues.push(["(-", value, ")"].join(''));
+    else if (!pressedEqual)
+        previewValues.push(value);
+    if (opCode != 'none') 
+        previewValues.push(opCode);
+}
+
 function setUpOperationButtons() {
     for (let [opCode, button] of Object.entries(elements.operationButtons))
         button.addEventListener("click", function () {
             let sign = 1;
             let text = elements.display.textContent;
+            let opCodeToStore = opCode;
+            let previousSign = 1;
+            if (stored)
+                previousSign = stored.sign;
             if (stored && stored.text && stored.opCode)
                 text = calculate();
             if (opCode == '-' && !pressedDigit) {
                 sign = -1;
                 text = stored ? stored.text : null;
-                opCode = lastOpPressed ? lastOpPressed : null;
+                opCodeToStore = lastOpPressed ? lastOpPressed : null;
             }
             if (stored && stored.text === null && stored.sign == -1)
                 // We are in the case where we have a negative number
                 // at the beginning, so we 
                 text = '-'.concat(elements.display.textContent);
+            addPreviewValues(previousSign, opCodeToStore);
             stored = {
                 text,
-                opCode,
+                opCode: opCodeToStore,
                 sign,
             };
+            elements.preview.style.display = "block";
+            elements.preview.textContent = previewValues.join('');
             pressedEqual = false;
             pressedDigit = false;
-            lastOpPressed = opCode;
+            lastOpPressed = opCodeToStore;
             lastSign = sign;
             elements.display.textContent = "";
         });
@@ -116,6 +143,9 @@ function setUpOperationButtons() {
 
 function setUpCalculateButton() {
     elements.calculateButton.addEventListener("click", function () {
+        if (!pressedEqual)
+            addPreviewValues(stored.sign, 'none');
+        elements.preview.style.display = "none";
         if (pressedEqual) {
             elements.display.textContent = calculateOnEqual();
             return;
